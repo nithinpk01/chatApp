b2b2de3d86207f0d2b53b5ddfbe49580f16c34da
nithinpkinfo <nithinpkinfo@gmail.com>
2022-11-06 14:45:13 +0530

    getchat persons

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,37 +1,37 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import profile from '../images/profile.png'
+import { doc, onSnapshot } from "firebase/firestore";
+import { db } from '../config/firebase';
+import { AuthContext } from '../context/AuthContext';
 
 export const Chats = () => {
+    const { currentUser } = useContext(AuthContext);
+    const [chats, setChats] = useState([]);
+    useEffect(() => {
+        const getChats = () => {
+            const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+                setChats(doc.data());
+            });
+            return () => {
+                unsub();
+            }
+        };
+        currentUser.uid && getChats();
+
+    }, [currentUser.uid])
     return (
+
         <div className='chats'>
-            <div className='userChat'>
-                <img src={profile} alt="" />
-                <div className="userChatInfo">
-                    <span>Nithin PK</span>
-                    <p>Hello</p>
+            {Object.entries(chats)?.map(chat => (
+                <div className='userChat' key={chat[0]}>
+                    <img src={chat[1].userInfo.photoURL} alt="" />
+                    <div className="userChatInfo">
+                        <span>{chat[1].userInfo.displayName}</span>
+                        <p>hello</p>
+                    </div>
                 </div>
-            </div>
-            <div className='userChat'>
-                <img src={profile} alt="" />
-                <div className="userChatInfo">
-                    <span>Nithin PK</span>
-                    <p>Hello</p>
-                </div>
-            </div>
-            <div className='userChat'>
-                <img src={profile} alt="" />
-                <div className="userChatInfo">
-                    <span>Nithin PK</span>
-                    <p>Hello</p>
-                </div>
-            </div>
-            <div className='userChat'>
-                <img src={profile} alt="" />
-                <div className="userChatInfo">
-                    <span>Nithin PK</span>
-                    <p>Hello</p>
-                </div>
-            </div>
+            ))}
         </div>
+
     )
 }
