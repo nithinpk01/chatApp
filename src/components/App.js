a9faa5277da8327c2a89ab334b2959dba84aa57a
nithinpkinfo <nithinpkinfo@gmail.com>
2022-12-04 09:54:17 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,7 @@ import { useContext } from 'react';
 import { AuthContext } from '../context/AuthContext';
 
 function App() {
+  //by nithin
   const { currentUser } = useContext(AuthContext)
   const ProtectedRoute = ({ children }) => {
     if (!currentUser) {
